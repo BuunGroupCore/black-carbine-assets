29967216bd436fa0f697e19b831d9f3da74744cb
Sacha Roussakis-Notter <sroussakisnotter@woolworths.com.au>
2024-05-11 00:13:38 +1000

    added images

--- server.js
+++ server.js
@@ -13,35 +13,6 @@ app.get('/', (req, res) => {
         .filter(file => file.endsWith('.jpg') || file.endsWith('.png') || file.endsWith('.gif'))
         .map(file => `<img src="/${file}" alt="${file}">`)
         .join('');
-
-    res.send(`
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Image Gallery</title>
-    <style>
-        body {
-            margin: 0;
-            height: 100vh;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-        }
-
-        .image-container {
-            text-align: center;
-        }
-    </style>
-</head>
-<body>
-    <div class="image-container">
-        <img src="/black-carbine.png" alt="Black Carbine Logo">
-    </div>
-</body>
-</html>
-    `);
 });
 
 const PORT = process.env.PORT || 3001;
