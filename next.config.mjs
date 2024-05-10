bc97b67b6b9e5ea3555203b72315f16ef9b72025
Sacha Roussakis-Notter <sroussakisnotter@woolworths.com.au>
2024-05-10 23:42:28 +1000

    added images

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: false,
-    output: "standalone",
-    pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-};
-
-export default nextConfig;
